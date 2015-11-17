561b556a8cee767cdb50de3c9b69335d4bbba3e1
ZachariasOctavious at codea-scm <ZachariasOctavious@codea-scm.aws.mapote.com>
2015-11-17 11:07:23 -0500

    initial commit

--- README.md
+++
@@ -1,4 +0,0 @@
-# TestAppGroup2
-
-Here is the GitHub SSH:
-git@github.com:MotherTeresaHS/TestAppGroup2.git
---
+++ tabs/ArtworkScene.lua
@@ -0,0 +1,14 @@
+ArtworkScene = class()
+
+function ArtworkScene:init()
+    -- you can accept and set parameters here
+  
+end
+
+function ArtworkScene:draw()
+    -- Codea does not automatically call this method
+end
+
+function ArtworkScene:touched(touch)
+    -- Codea does not automatically call this method
+end
